fdcbb530bb88a0d28f9495567df2ec79a565fc0c
HCarrell <carrhun@sterlinghs.org>
2018-01-23 08:07:12 -0600

    Canvas Resize

--- Aim_Booster.py
+++ Aim_Booster.py
@@ -4,7 +4,7 @@ import Tkinter
 root = Tkinter.Tk()
 root.wm_title('Color-Shape Art Creation')
 
-canvas = Tkinter.Canvas(root, height=300, width=300, background='#FFFFFF')
+canvas = Tkinter.Canvas(root, height=600, width=600, background='#FFFFFF')
 canvas.grid(row=0, column=1, rowspan=3)
 
 root.mainloop()
